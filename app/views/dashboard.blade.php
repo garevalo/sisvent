55fd6c90a1896deb9839de38ea00704cedf4a000
Giordan Arevalo <gbap0506@hotmail.com>
2015-02-07 20:16:41 -0500

    'dashborad'

--- app/views/dashboard.blade.php
+++ app/views/dashboard.blade.php
@@ -17,8 +17,13 @@
     <div class=row>
     	<div class="col-lg-3 col-sm-6 col-xs-12">
             <div class="databox radius-bordered databox-shadowed databox-graded">
+                
                 <div class="databox-left bg-lightred">
-
+                    <div class="databox-piechart">
+                        <span class="white font-90">
+                            <i class="fa fa-cog fa-3x"></i>
+                        </span>
+                    </div>
                 </div>
                 <div class="databox-right">
                     <span class="databox-number lightred">Productos</span>
@@ -33,7 +38,11 @@
         <div class="col-lg-3 col-sm-6 col-xs-12">
             <div class="databox radius-bordered databox-shadowed databox-graded">
                 <div class="databox-left bg-lightred">
-
+                    <div class="databox-piechart">
+                        <span class="white font-90">
+                            <i class="fa fa-users fa-3x"></i>
+                        </span>
+                    </div>
                 </div>
                 <div class="databox-right">
                     <span class="databox-number lightred">Usuarios</span>
@@ -174,22 +183,24 @@
             </div>
         </div>
         
-        <div class="col-lg-3 col-sm-6 col-xs-12">
-            <div class="databox radius-bordered databox-shadowed databox-graded">
-                <div class="databox-left bg-sky">
-                    hgfkj dhgkjkfdghkjdfhgjkfdhkjghdfkghdfgkjdfhgf g
-                    dfksjlfdjdsfkjs f sdjflsdk
+       <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="databox bg-white radius-bordered">
+                <div class="databox-left bg-themesecondary">
+                    <div class="databox-piechart">
+                        <span class="white font-90">
+                            <i class="fa fa-automobile fa-3x"></i>
+                        </span>
+                    </div>
                 </div>
                 <div class="databox-right">
-                    <span class="databox-number lightred">322</span>
-                    <div class="databox-text darkgray">Registro de Productos</div>
-                    <div class="databox-stat bg-lightred radius-bordered">
-                        <div class="stat-text">4% </div>
-                        <i class="stat-icon fa fa-arrow-down"></i>
+                    <span class="databox-number themesecondary">28</span>
+                    <div class="databox-text darkgray">NEW TASKS</div>
+                    <div class="databox-stat themesecondary radius-bordered">
+                        <i class="stat-icon icon-lg fa fa-tasks"></i>
                     </div>
                 </div>
             </div>
-        </div>
+      </div>
     </div>
 @stop
 
