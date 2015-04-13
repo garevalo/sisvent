9356ead4a12bc512ab14b5e9944ca995e54dae89
giordan.arevalo <giordan.arevalo@ec.pe>
2015-04-13 18:18:42 -0500

    avance

--- app/config/mail.php
+++ app/config/mail.php
@@ -28,7 +28,7 @@ return array(
 	|
 	*/
 
-	'host' => 'smtp.mailgun.org',
+	'host' => 'smtp.gmail.com',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => null, 'name' => null),
+	'from' => array('address' => 'zagha.s@gmail.com', 'name' => 'giordan brian'),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -80,7 +80,7 @@ return array(
 	|
 	*/
 
-	'username' => null,
+	'username' => zagha.s@gmail.com,
 
 	/*
 	|--------------------------------------------------------------------------
